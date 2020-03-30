827192c1ac14d726805cf82173f58b7f64f6fc04
Ashir <ashirq@outlook.com>
2020-03-30 18:38:16 +0100

    The app now returns a message telling who won and what they won with.

--- controller.rb
+++ controller.rb
@@ -3,3 +3,19 @@ require('sinatra/contrib/all') if development?
 
 require_relative('./models/rock_paper_scissors')
 also_reload('./models/*')
+
+get '/play/:p1/:p2' do
+  p1_choice = params['p1']
+  p2_choice = params['p2']
+  @result = RockPaperScissors.check_winner(p1_choice, p2_choice)
+  @picture = 
+  erb(:result)
+end
+
+get '/about' do
+  erb(:about)
+end
+
+get '/' do
+  erb(:home)
+end
--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -3,28 +3,28 @@ class RockPaperScissors
   def self.check_winner(p1, p2)
     p1_choice = p1.downcase()
     p2_choice = p2.downcase()
-    return "The result is a draw" if p1_choice == p2_choice
+    return "a draw with both players choosing #{p1_choice}!" if p1_choice == p2_choice
 
     if p1_choice == 'rock'
-      return result_message('player1', p1_choice) if p2_choice == 'scissors'
-      return result_message('player2', p2_choice) # happens if player2 = paper
+      return result_message('player 1', p1_choice) if p2_choice == 'scissors'
+      return result_message('player 2', p2_choice) # happens if player2 = paper
     end
 
     if p1_choice == 'paper'
-      return result_message('player1', p1_choice) if p2_choice == 'rock'
-      return result_message('player2', p2_choice) # happens if player2 is scissors
+      return result_message('player 1', p1_choice) if p2_choice == 'rock'
+      return result_message('player 2', p2_choice) # happens if player2 is scissors
     end
 
     if p1_choice == 'scissors'
-      return result_message('player1', p1_choice) if p2_choice == 'paper'
-      return result_message('player2', p2_choice) # happens if player2 is rock
+      return result_message('player 1', p1_choice) if p2_choice == 'paper'
+      return result_message('player 2', p2_choice) # happens if player2 is rock
     end
   end
 
   # The result_message method puts together the result_message to convey the result
   # of the game
   def self.result_message(player, choice)
-    return "#{player} won with #{choice}"
+    return "#{player} won with #{choice}!"
   end
 
 
